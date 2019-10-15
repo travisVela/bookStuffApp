0f5172133f5626195c1f4933ef2f6723780fea40
travis-vela <52721388+travis-vela@users.noreply.github.com>
2019-10-15 15:07:54 -0500

    added tabs to the places page. moved routing for the tabs to a places-routing page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
 ];
 
 @NgModule({
