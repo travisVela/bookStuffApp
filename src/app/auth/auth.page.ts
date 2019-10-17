ebd6e595f08efbb1994763a090f3daf659ab77ed
travis-vela <52721388+travis-vela@users.noreply.github.com>
2019-10-17 10:26:58 -0500

    extract offers page list items and style independantly. add form to login page. add validation to form.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,8 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
+
   constructor(
     private authService: AuthService,
     private router: Router,
@@ -33,5 +36,24 @@ export class AuthPage implements OnInit {
       })
     this.authService.login();
   }
+
+  onToSignUp() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password)
+
+    if (this.isLogin) {
+
+    }
+  }
+
 
 }
