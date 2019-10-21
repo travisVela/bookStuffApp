f486e17364253a36ad1a3d891675781bfbfc7beb
Travis Vela <jtravisvela@gmail.com>
2019-10-21 08:24:13 -0500

    Summery

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Keys } from '../../../../src/keys';
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
 
-  constructor(private modalCtrl: ModalController) { }
-
+  constructor(
+    private modalCtrl: ModalController,
+    private Keys: Keys
+  ) { }
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.getGoogleMaps().then(googleMaps => {
+
+    }).catch(err => {
+      console.log(err);
+    });
+  }
 
   onCancel() {
     this.modalCtrl.dismiss();
   }
 
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.map) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise ((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${Keys.googleMapsTokens}`;
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available.');
+        }
+      };
+    });
+  }
 }
