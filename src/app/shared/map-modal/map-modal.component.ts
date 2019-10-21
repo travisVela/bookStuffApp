a36fa27956b01659bcebfb116759cb92ab5982bc
travis-vela <52721388+travis-vela@users.noreply.github.com>
2019-10-21 09:57:06 -0500

    add google map to the map modal component. add event listener to apply visible class to map after page has loaded

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { Keys } from '../../../../src/keys';
+import { googleMapsToken } from '../../../../src/keys';
 
 @Component({
   selector: 'app-map-modal',
@@ -8,16 +8,24 @@ import { Keys } from '../../../../src/keys';
   styleUrls: ['./map-modal.component.scss'],
 })
 export class MapModalComponent implements OnInit, AfterViewInit {
-
+  @ViewChild('map', {static: false}) mapElementRef: ElementRef; 
+  token = googleMapsToken;
   constructor(
     private modalCtrl: ModalController,
-    private Keys: Keys
+    private renderer: Renderer2
   ) { }
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.getGoogleMaps().then(googleMaps => {
-
+      const mapEl = this.mapElementRef.nativeElement;
+      const map = new googleMaps.Map(mapEl, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 16
+      });
+      googleMaps.event.addListenerOnce(map, 'idle', () => {
+        this.renderer.addClass(mapEl, 'visible');
+      });
     }).catch(err => {
       console.log(err);
     });
@@ -35,7 +43,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     }
     return new Promise ((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${Keys.googleMapsTokens}`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${this.token}`;
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
