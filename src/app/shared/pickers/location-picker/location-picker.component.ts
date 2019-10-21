f77149cf016ecac50ad82bb78bddaf6dcdede330
travis-vela <52721388+travis-vela@users.noreply.github.com>
2019-10-21 10:50:18 -0500

    add geo location api to transform lat and long to physical address of selected location

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { HttpClient } from '@angular/common/http';
+import { googleMapsToken } from '../../../../../src/keys';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-location-picker',
@@ -8,18 +11,39 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
   styleUrls: ['./location-picker.component.scss'],
 })
 export class LocationPickerComponent implements OnInit {
-
-  constructor(private modalCtrl: ModalController) { }
+  token = googleMapsToken;
+  constructor(
+    private modalCtrl: ModalController,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {}
 
   onPickLocation() {
     this.modalCtrl.create({component: MapModalComponent})
-    .then(modalEl => {
+    .then(modalEl => {  
       modalEl.onDidDismiss().then(modalData => {
-        console.log(modalData.data);
+        if (!modalData.data) {
+          return;
+        }
+        this.getAddress(modalData.data.lat, modalData.data.lng)
+          .subscribe((address) => {
+            console.log(address);
+          });
       })
       modalEl.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http.get<any>(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.token}`
+    ).pipe(map(geoData => {
+      console.log(geoData);
+      if (!geoData || !geoData.results || geoData.results.length === 0) {
+        return null;
+      }
+      return geoData.results[0].formatted_address;
+    }));
   }
 }
