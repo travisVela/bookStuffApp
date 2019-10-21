fa56b8dceaa13c702e6caf7f633d07ec1004a1a9
travis-vela <52721388+travis-vela@users.noreply.github.com>
2019-10-21 11:25:41 -0500

    add method for static map image. add image to the template. add spinner to template to display while image is loading

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -3,7 +3,10 @@ import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { googleMapsToken } from '../../../../../src/keys';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from '../../../places/loaction.model';
+import { NumberValueAccessor } from '@angular/forms';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -12,6 +15,9 @@ import { map } from 'rxjs/operators';
 })
 export class LocationPickerComponent implements OnInit {
   token = googleMapsToken;
+  selectedLocationImage: string;
+  isLoading = false;
+
   constructor(
     private modalCtrl: ModalController,
     private http: HttpClient
@@ -26,15 +32,29 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng)
-          .subscribe((address) => {
-            console.log(address);
-          });
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImage: null
+        };
+        this.isLoading = true;
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(this.getStaticImage(pickedLocation.lat, pickedLocation.lng, 14))
+          })
+        ).subscribe(staticMapImageUrl => {staticMapImageUrl;
+          pickedLocation.staticMapImage = staticMapImageUrl;
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
+        })
       })
       modalEl.present();
     });
   }
 
+  // seperate request to get the address using google geo loaction
   private getAddress(lat: number, lng: number) {
     return this.http.get<any>(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.token}`
@@ -45,5 +65,11 @@ export class LocationPickerComponent implements OnInit {
       }
       return geoData.results[0].formatted_address;
     }));
+  }
+
+  // seperate get request to get the static image of selected location
+  private getStaticImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}
+      &key=${this.token}`
   }
 }
